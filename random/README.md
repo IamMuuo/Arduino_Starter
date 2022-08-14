d38b9a682f3bfbb7fdb23e0f0424dcb075a0cb46
erick <hearteric57@gmail.com>
2022-08-14 23:06:47 +0300

    Added project random README

--- random/README.md
+++ random/README.md
@@ -1,3 +1,12 @@
 # Random!
 Denostrates generation of random number generation
 
+Classic guess the number game with arduino
+
+## Equipment Required
+* Arduino Uno or Similar
+
+## Circuit Design
+***
+
+<img src="../arduino_uno.png" height="300">
--- random/random.ino
+++ random/random.ino
@@ -7,6 +7,8 @@
 
 #define BAUD 19200
 
+int guess, number, counter;
+
 void setup()
 {
     Serial.begin(BAUD); // set the baud rate for serial printing
@@ -19,10 +21,27 @@ void setup()
 void loop()
 {
     // generate random numbers
+    for(counter = 0; counter < 3; counter++)
+    {
+        Serial.println("I am thinking of a number between 0 - 10\n"
+                "try to guess what it is: ");
 
-    int r = rand() % 10;
+        while(Serial.available() > 0)
+        {
+            guess = Serial.read() - '0';
+        }
 
-    Serial.print("A random number between 0 and 10: ");
-    Serial.println(r);
+        if(guess != number)
+        {
+            Serial.print("You failed to guess the number please try again\n"
+                    "Attempts remaining: ");
+            Serial.println(counter);
+        }
+        else
+        {
+            Serial.print("You guessed the number correctly!\nCongrats!!!");
+            break;
+        }
+    }
 }
 
